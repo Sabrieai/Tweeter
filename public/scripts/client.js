15a28ace7d319f807cecfa0a4570b3ba665e6b0d
Aisha Sabrie <sabriealt@gmail.com>
2021-12-16 19:10:42 -0500

    remove driver and test code from jquery document ready function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -12,35 +12,7 @@
 
 // eslint-disable-next-line no-undef
 $(document).ready(function() {
-  ///////////////////////////// DATATBASES FOR TESTING //////////////////////////
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ];
-
-  ////////////////////////////// MY CODE ////////////////////////////////////////
-
+  
   const renderTweets = function(tweets) {
     // loops through tweets (for in because its an object)
     for (let tweet in tweets) {
@@ -80,7 +52,7 @@ $(document).ready(function() {
     return $markUp;
    
   };
-  renderTweets(data);
+  //renderTweets(data);
 
   // uses ajax call to submit form and prevent page reload
   // console.log to make sure we are recieving a serialized version
@@ -96,7 +68,6 @@ $(document).ready(function() {
     const chars = $("#tweet-text").val().length;
     if (chars === 0 || data === null) {
       alert("Your tweet is empty, show us what you've got!");
-      alert("Hello! I am an alert box!");
       validTweet = false;
     } else if (chars > 140) {
       alert("Your tweet exeeds the limit, try and keep the counter above 0");
@@ -129,18 +100,6 @@ $(document).ready(function() {
   loadTweets();
   
 });
-
-////////////////////////////// TEST CODE ////////////////////////////////////////
-
-//const $tweet = createTweetElement(tweetData);
-//console.log($tweet); // to see what it looks like
-//const ready = $('#tweets-container').append($tweet);
-
-//$(document).ready(function() {
-// $('#tweets-container').append($tweet);
-//});
-
-//const check = renderTweets(data);
 
 
 
